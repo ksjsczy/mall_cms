0ba4c4aee857d59aaba792b5a4eaaa9622039a24
ksjsczy <1361543489@qq.com>
2022-05-23 12:56:59 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,17 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'APP',
 
+
+
+
   components: {
-    HelloWorld
+    HelloWorld,
   }
 })
 </script>
